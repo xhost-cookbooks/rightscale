04e5be75501dd0746c397de8f8571f90a403666e
Chris Fordham <chris@xhost.com.au>
2013-10-16 11:27:25 +1100

    Update maintainer_email and bump version in metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 maintainer       "Chris Fordham"
-maintainer_email "chris@xhost.com.au"
+maintainer_email "chris@fordham-nagy.id.au"
 license          "Apache 2.0"
 description      "Installs/Configures a Rightscale Server node."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 depends "collectd"
 depends "rest_connection"
