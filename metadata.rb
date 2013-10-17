4a06691b9c2460ab7e0f96b79cc25d151f5cb910
Chris Fordham <chris@xhost.com.au>
2013-10-18 09:21:23 +1100

    Add name to metadata.rb, improve description and bump version to 0.1.2.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
+name             "rightscale"
 maintainer       "Chris Fordham"
 maintainer_email "chris@fordham-nagy.id.au"
 license          "Apache 2.0"
-description      "Installs/Configures a Rightscale Server node."
+description      "Sets up a Rightscale Server node."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.2"
 
 depends "collectd"
 depends "rest_connection"
